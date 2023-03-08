f48a546d701d54a85244a2eadf476e7a616a9d71
ZuzaBedkowska <zbedkowska@student.agh.edu.pl>
2023-03-08 15:45:49 +0100

    Dodanie okienka

---
+++ src/main/java/ui/MainUI.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+
+public class MainUI {
+    private JPanel rootPanel;
+    private JComboBox<String> displayBox;
+    private JButton displayButton;
+    private JTable showTable;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton removeButton;
+    public JPanel getRootPanel(){
+        return rootPanel;
+    }
+}
