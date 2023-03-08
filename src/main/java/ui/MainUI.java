17115219408eda3141c4884dea1de67588a25241
ZuzaBedkowska <zbedkowska@student.agh.edu.pl>
2023-03-08 18:19:57 +0100

    Dodanie tabeli do GUI

--- src/main/java/ui/MainUI.java
+++ src/main/java/ui/MainUI.java
@@ -1,6 +1,42 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+
+class DataFetcher extends AbstractTableModel {
+
+    private final String[] columnNames;
+    ArrayList<Object[]> userData;
+
+    public DataFetcher() {
+        columnNames = new String[]{"Czynność", "Czynność bezpośrednio poprzedzająca", "Czas trwania"};
+        userData = new ArrayList<>();
+    }
+
+    @Override
+    public int getRowCount() {
+        return userData.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+
+    public String[] getColumnNames() {
+        return columnNames;
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+}
 
 public class MainUI {
     private JPanel rootPanel;
@@ -10,7 +46,18 @@ public class MainUI {
     private JButton addButton;
     private JButton editButton;
     private JButton removeButton;
+    private DataFetcher dataFetcher;
+    public MainUI() {
+        editButton.setEnabled(false);
+        removeButton.setEnabled(false);
+        dataFetcher = new DataFetcher();
+        showEmpty();
+    }
     public JPanel getRootPanel(){
         return rootPanel;
     }
+    public void showEmpty() {
+        showTable.setModel(dataFetcher);
+    }
+
 }
